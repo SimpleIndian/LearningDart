76d19e299c72635cb795e4000e10faf790bf94d1
souvik <mandalsouvik362@gmail.com>
2020-01-24 17:04:32 +0530

    Priamry Datatypes in Dart

---
+++ DataTypes.dart
@@ -0,0 +1,21 @@
+//Priamry Datatypes in Dart
+
+void main(){
+      //Numbers
+  int myage = 17;
+//or
+  var age = 17;
+
+      
+      //string 
+  String myname = "souvik";
+//or
+  var name = "souvik";
+
+  
+      //boolean
+  bool isAlive = true;
+//or
+  var Alive = true;
+  
+} 
