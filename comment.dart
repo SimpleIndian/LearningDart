77225cf67a40c05ef72c2ec68ef16bdad7069c02
souvik <mandalsouvik362@gmail.com>
2020-01-24 16:35:14 +0530

    Comments in Dart

---
+++ comment.dart
@@ -0,0 +1,20 @@
+void main(){
+  print("It is a comment");//this is not ignored by the compiler
+
+
+  //                               but this will ignored by the compiler
+
+  //it is a real comment in dart
+  
+  //              to use comment you have to use '/comment/'<--this is for single line command
+
+
+  //                            or 
+  
+  
+  //               '/*comment*/<-- this is for multiple line command
+  
+
+}
+
+//comment outside any method
