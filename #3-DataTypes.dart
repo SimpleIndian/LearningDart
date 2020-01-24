8dc0c9ec01defda6e863d143be53c491525d0d61
souvik362 <58888934+souvik362@users.noreply.github.com>
2020-01-24 17:31:25 +0530

    Primary DataTypes in Dart

--- #3-DataTypes.dart
+++ #3-DataTypes.dart
@@ -1,28 +1,28 @@
-//Priamry Datatypes in Dart
+                                //Primary Datatypes in Dart
 
 void main(){
-      //integer Numbers
+      //integer Numbers .
   int myage = 17;
 //or
-  var age = 17; //it is defined integer by the Compiler
- int Hexvalue =0xEDFABEEC; //we define Hex value using integer varriable
+  var age = 17; //it is defined integer by the Compiler .
+ int Hexvalue =0xEDFABEEC; //we define Hex value using integer varriable.
 
   double myGrade = 7.9;
   //or
-  var grade = 7.9;//it is defined double by the Compiler
-  double ExponentValue = 25e6; //we define exponent using double varriable
+  var grade = 7.9;//it is defined double by the Compiler.
+  double ExponentValue = 25e6; //we define exponent using double varriable.
   
       //string 
   String myname = "souvik";
 //or
-  var name = "souvik"; //it is defined string by the Compiler
+  var name = "souvik"; //it is defined string by the Compiler.
 
   
       //boolean
   bool isAlive = true;
 //or
-  var Alive = true; //it is defined boolean by the Compiler
+  var Alive = true; //it is defined boolean by the Compiler.
  
 } 
- //All datatypes in dart are Objects
- //Therefore their initial value is 'null' by default
+ //All datatypes in dart are Objects .
+ //Therefore their initial value is 'null' by default .
