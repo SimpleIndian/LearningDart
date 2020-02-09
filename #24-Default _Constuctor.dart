5f649efa33a51cde46d94a4338a63c4808003bbf
Souvik Mandal <58888934+souvik362@users.noreply.github.com>
2020-02-09 13:50:37 +0530

    Constructors in Dart

---
+++ #24-Default _Constuctor.dart
@@ -0,0 +1,47 @@
+void main(){
+  //Object oriented Programming in Dart
+                   //Constructor in Dart
+  
+  
+  //Constructor  is special method, it Can create object or it is executed when we the object of a class is created.
+  
+  /*           Types of Constructor
+   * 1 Default Constructor
+   * 2 parameterized Constructor
+   * 3 Named Constructor
+   * 4 Constant Constructor  
+   */
+
+                   //Default Constuctor in Dart
+  
+  var student1 = Student();//Default Constructor 
+  //it is Called default constructor if we add executable code in constructor it will execute 
+  //every time we create object from class, see below in class -->
+  
+  student1.name = "souvik"; 
+  student1.id = 23;
+  print("${student1.id} > ${student1.name}");
+  student1.sleep(); 
+  
+ 
+ var student2 = Student();
+  student2.name = "sam";
+  print(student2.name);
+  
+  
+  //actually objects inherit the properties and method / behavior  of a class
+}
+
+
+//we can't create default Constructor and Parameterized Constructor in the same class
+class Student{
+  int id; 
+  String name;
+  
+  Student(){ //we add executable code in default constuctor.
+    print("Hello Constuctor--> this time is constuctor executed");
+  }
+  void sleep(){
+    print("${this.name}'s sleep too much");
+  }
+}
---
+++ #26-Named_Constructor.dart
@@ -0,0 +1,35 @@
+void main(){
+  //Object oriented Programming in Dart
+                   //ConstructOr in Dart
+  
+  
+  //Constructor  is special method,it Can create object or it is executed when we the object of a class is created.
+  
+  /*           Types of Constructor
+   * 1 Default Constructor
+   * 2 parameterized Constructor
+   * 3 Named Constructor
+   * 4 Constant Constructor  <!--it is basically important for GUI --!> 
+   */
+  
+                  //Named constructor in Dart
+ var student1 = Student.myNamedConstructor(23,"Souvik");
+  print("${student1.id} and ${student1.name}");
+}
+
+
+//we can't create default Constructor and Parameterized Constructor in the same class.
+//But we can create Named Constructor and Paramerterized Constructor in the same class as Many as we want
+class Student{
+  int id; 
+  String name;
+  /*
+   Student.myNamedConstructor(){
+     print("Hello Named Constructor ");//Normal Named Constuctor
+   }*/
+  
+  Student.myNamedConstructor(this.id, this.name);//Named Constuctor with Parameter
+  void sleep(){
+    print("${this.name}'s sleep too much");
+  }
+}
